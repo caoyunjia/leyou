12fb503e0716ff4e52f9674cab4e5ac94c981274
caoyunjia <630335892@qq.com>
2019-08-20 21:58:44 +0800

    网关的新增

---
+++ ly-user/ly-user-interface/src/main/java/com/leyou/user/api/UserApi.java
@@ -0,0 +1,12 @@
+package com.leyou.user.api;
+
+import com.leyou.user.pojo.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface UserApi {
+    @GetMapping("/user/query")
+    User queryUserByUsernameAndPassword(@RequestParam(value = "username")String username,
+                                                        @RequestParam(value = "password")String password);
+
+}
