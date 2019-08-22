12f3fe6e395fe8e2f9d4501f5d4764c2ecb1694b
caoyunjia <630335892@qq.com>
2019-08-22 22:34:18 +0800

    去除fegin的前缀

--- ly-user/ly-user-interface/src/main/java/com/leyou/user/api/UserApi.java
+++ ly-user/ly-user-interface/src/main/java/com/leyou/user/api/UserApi.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface UserApi {
-    @GetMapping("/user/query")
+    @GetMapping("/query")
     User queryUserByUsernameAndPassword(@RequestParam(value = "username")String username,
                                                         @RequestParam(value = "password")String password);
 
