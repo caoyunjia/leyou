31012834ff65f5abc0d2670c6c2b81fa30c4c638
caoyunjia <630335892@qq.com>
2019-10-10 21:04:17 +0800

    JsonIgnore引入正确的包引入baseMapper的改造

--- ly-item/ly-item-serivce/src/main/java/com/leyou/item/mapper/SpecParamMapper.java
+++ ly-item/ly-item-serivce/src/main/java/com/leyou/item/mapper/SpecParamMapper.java
@@ -3,5 +3,5 @@ package com.leyou.item.mapper;
 import com.leyou.common.mapper.BaseMapper;
 import com.leyou.item.pojo.SpecParam;
 
-public interface SpecParamMapper extends BaseMapper<SpecParam,Long> {
+public interface SpecParamMapper extends BaseMapper<SpecParam> {
 }
